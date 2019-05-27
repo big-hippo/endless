79bdbd5b758b16c27e449242036d6ce0a4c3869b
big-hippo <1525980920@qq.com>
2019-05-27 18:52:12 +0800

    fix for autocert

--- endless.go
+++ endless.go
@@ -254,10 +254,20 @@ func (srv *endlessServer) ListenAndServeTLS(certFile, keyFile string) (err error
 		config.NextProtos = []string{"http/1.1"}
 	}
 
-	config.Certificates = make([]tls.Certificate, 1)
-	config.Certificates[0], err = tls.LoadX509KeyPair(certFile, keyFile)
-	if err != nil {
-		return
+// 	config.Certificates = make([]tls.Certificate, 1)
+// 	config.Certificates[0], err = tls.LoadX509KeyPair(certFile, keyFile)
+// 	if err != nil {
+// 		return
+// 	}
+	
+	configHasCert := len(config.Certificates) > 0 || config.GetCertificate != nil
+	if !configHasCert || certFile != "" || keyFile != "" {
+		var err error
+		config.Certificates = make([]tls.Certificate, 1)
+		config.Certificates[0], err = tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			return err
+		}
 	}
 
 	go srv.handleSignals()
